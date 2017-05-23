ed1e4acdeb8322148f5e700f760b605747283fb6
Bruno Henrique Paiva de Campos <ra195080@zecolmeia.lab.ic.unicamp.br>
2017-05-23 16:57:31 -0300

    lab15B

---
+++ lab15B/lab15B.c
@@ -0,0 +1,28 @@
+/* Lab 15B
+ * Nome: Bruno Campos;
+ * RA: 195080;
+ */
+
+#include <stdio.h>
+#include <string.h>
+int removePadrao(char str[], char padrao[], char targetStr[]);
+int removeBloco(char str[], char c, char targetStr[]);
+int substituiPadrao(char str[], char padrao[], char novoPadrao[], char targetStr[]);
+
+fgets(seq, 30, stdin);
+seq[strlen(seq)-1] = '\0';
+
+int removePadrao (char str[], char padrao[], char targetStr[]) {
+    
+    printf("removePadrao(%s): %s\n", padrao, novaSeq);
+}
+
+int removeBloco (char str[], char c, char targetStr[]) {
+    
+    printf("removeBloco(*): %s\n", novaSeq);
+}
+
+int substituiPadrao (char str[], char padrao[], char novoPadrao[], char targetStr[]) {
+    
+    printf("substituiPadrao(%s, %s): %s\n", padrao, novoPadrao, novaSeq);
+}
